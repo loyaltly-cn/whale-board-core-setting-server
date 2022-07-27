c4648d22ff9ccec5acb3a9b3fabed7acfbdd7955
loyal <102851403+loyaltly-cn@users.noreply.github.com>
2022-07-27 10:45:03 +0800

    Update server.py

--- core_setting/server.py
+++ core_setting/server.py
@@ -1,26 +1,32 @@
 import flask
 import flask_cors as cors
-import core
+import cmd
+import json
 from flask import request
+from file import file
 
 server = flask.Flask(__name__)
 
 cors.CORS(server)
 
+f = file()
+
 @server.route('/connect',methods = ['get'])
 def connect():
-
+    cmd.open()
     return 'connect successful'
-
-@server.route('/open',methods = ['get'])
-def open():
-    core.open()
-    return 'open suceesful'
 
 @server.route('/close',methods = ['get'])
 def close():
-    core.close()
+    cmd.close()
     return 'close successful'
+
+@server.route('/update',methods = ['post'])
+def update():
+    obj = request.data
+    obj = json.loads(obj)
+    f.update(obj['type'],obj['name'],obj['data'])
+    return 'update success'
 
 
 if __name__ == '__main__':
